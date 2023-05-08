a3eeca6f8080a6a140cd25a1f81b42a337ecfa15
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-05-08 00:37:28 -0600

    Mejoras en las 4 funciones
    ~Para compilar y ejecutar
    g++ -o server server.cpp -lwthttp -lwt
    ./server --docroot /ruta/a/carpeta/con/archivos/ server.cpp --http-address=0.0.0.0 --http-port=8080
    
    ~Para detener el proceso:
    sudo lsof -i :8080
    kill numprocesoindicadoporcomandoanterior

--- web/server.cpp
+++ web/server.cpp
@@ -14,7 +14,10 @@ private:
     Wt::WLineEdit* secondNumberEdit_;
     Wt::WText* resultText_;
 
-    void calculate();
+    void add();
+    void subtract();
+    void multiplication();
+    void division();
 };
 
 CalculatorApp::CalculatorApp(const Wt::WEnvironment& env)
@@ -29,15 +32,25 @@ CalculatorApp::CalculatorApp(const Wt::WEnvironment& env)
     auto secondNumberLabel = container->addWidget(std::make_unique<Wt::WText>("Second number: "));
     secondNumberEdit_ = container->addWidget(std::make_unique<Wt::WLineEdit>());
 
-    auto calculateButton = container->addWidget(std::make_unique<Wt::WPushButton>("Calculate"));
-    calculateButton->clicked().connect(this, &CalculatorApp::calculate);
+    auto addButton = container->addWidget(std::make_unique<Wt::WPushButton>("+"));
+    addButton->clicked().connect(this, &CalculatorApp::add);
+
+    auto subtractButton = container->addWidget(std::make_unique<Wt::WPushButton>("-"));
+    subtractButton->clicked().connect(this, &CalculatorApp::subtract);
+
+    auto multiplicationButton = container->addWidget(std::make_unique<Wt::WPushButton>("*"));
+    multiplicationButton->clicked().connect(this, &CalculatorApp::multiplication);
+
+    auto divisionButton = container->addWidget(std::make_unique<Wt::WPushButton>("/"));
+    divisionButton->clicked().connect(this, &CalculatorApp::division);
 
     resultText_ = container->addWidget(std::make_unique<Wt::WText>());
 
     setTitle("Calculator");
 }
 
-void CalculatorApp::calculate()
+
+void CalculatorApp::add()
 {
     // Get the numbers from the UI
     double firstNumber = std::stod(firstNumberEdit_->text().toUTF8());
@@ -49,6 +62,47 @@ void CalculatorApp::calculate()
     // Update the UI with the result
     resultText_->setText("Result: " + std::to_string(result));
 }
+
+void CalculatorApp::subtract()
+{
+    // Get the numbers from the UI
+    double firstNumber = std::stod(firstNumberEdit_->text().toUTF8());
+    double secondNumber = std::stod(secondNumberEdit_->text().toUTF8());
+
+    // Calculate the result
+    double result = firstNumber - secondNumber;
+
+    // Update the UI with the result
+    resultText_->setText("Result: " + std::to_string(result));
+}
+
+void CalculatorApp::multiplication()
+{
+    // Get the numbers from the UI
+    double firstNumber = std::stod(firstNumberEdit_->text().toUTF8());
+    double secondNumber = std::stod(secondNumberEdit_->text().toUTF8());
+
+    // Calculate the result
+    double result = firstNumber * secondNumber;
+
+    // Update the UI with the result
+    resultText_->setText("Result: " + std::to_string(result));
+}
+
+void CalculatorApp::division()
+{
+    // Get the numbers from the UI
+    double firstNumber = std::stod(firstNumberEdit_->text().toUTF8());
+    double secondNumber = std::stod(secondNumberEdit_->text().toUTF8());
+
+    // Calculate the result
+    double result = firstNumber / secondNumber;
+
+    // Update the UI with the result
+    resultText_->setText("Result: " + std::to_string(result));
+}
+
+
 
 int main(int argc, char** argv)
 {
